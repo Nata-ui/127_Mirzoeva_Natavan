ac7f5a32f1e35dcd71612bc77f37f8a7677fd960
jeerryy <90263044+Nata-ui@users.noreply.github.com>
2022-03-27 21:35:06 +0300

    hw

---
+++ 2_sem/hw/Task2/src/Task2_Tests.java
@@ -0,0 +1,16 @@
+
+
+import org.junit.*;
+public class Task2_Tests extends Assert {
+    @Test
+    public void bubbleSort_CreateMyBubbleSort_TryToSort() {
+        BubbleSort<Integer> sorter = new BubbleSort<>();
+
+        MyComparator comparator = new MyComparator() {
+            @Override
+            public int compare(Object first, Object second) {
+                return 0;
+            }
+        };
+    }
+}
