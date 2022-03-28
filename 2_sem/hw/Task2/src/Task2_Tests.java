6cf0c9b8dbf9aa9c271bb95a8cd2b49852d99d25
jeerryy <90263044+Nata-ui@users.noreply.github.com>
2022-03-28 22:04:43 +0300

    создала тест BubbleSort_sortArray_sort и проверила на ошибки

--- 2_sem/hw/Task2/src/Task2_Tests.java
+++ 2_sem/hw/Task2/src/Task2_Tests.java
@@ -10,10 +10,18 @@ public class Task2_Tests extends Assert {
         }
     }
     @Test
-    public void BubbleSort_sortElement_sort() {
+    public void BubbleSort_BubbleSort_sosortElement_sort() {
         Integer[] data = {1};
         BubbleSort<Integer> bbs = new BubbleSort<Integer>();
         bbs.sort(data, new TestComparator());
         assertArrayEquals(new Integer[]{1}, data);
     }
+    @Test
+    public void BubbleSort_sortArray_sort() {
+        Integer[] data = {1, 2, 3, 4, 5, 6, 7};
+        BubbleSort<Integer> bbs = new BubbleSort<Integer>();
+        bbs.sort(data, new TestComparator());
+        assertArrayEquals(new Integer[]{1, 2, 3, 4, 5, 6, 7}, data);
+    }
+
 }
