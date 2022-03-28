fce8508756ad14061e60adcfbc824ad0426f7450
jeerryy <90263044+Nata-ui@users.noreply.github.com>
2022-03-28 23:25:26 +0300

    создала тест InsertionSort_Random_sort и проверила ошибки

--- 2_sem/hw/Task2/src/Task2_Tests.java
+++ 2_sem/hw/Task2/src/Task2_Tests.java
@@ -9,6 +9,7 @@ public class Task2_Tests extends Assert {
             return first - second;
         }
     }
+
     @Test
     public void BubbleSort_SortElement_sort() {
         Integer[] data = {1};
@@ -16,6 +17,7 @@ public class Task2_Tests extends Assert {
         bbs.sort(data, new TestComparator());
         assertArrayEquals(new Integer[]{1}, data);
     }
+
     @Test
     public void BubbleSort_sortArray_sort() {
         Integer[] data = {1, 2, 3, 4, 5, 6, 7};
@@ -23,6 +25,7 @@ public class Task2_Tests extends Assert {
         bbs.sort(data, new TestComparator());
         assertArrayEquals(new Integer[]{1, 2, 3, 4, 5, 6, 7}, data);
     }
+
     @Test
     public void BubbleSort_Random_sort() {
         Integer[] data = {90, 253, 20, 167, 18976};
@@ -30,6 +33,7 @@ public class Task2_Tests extends Assert {
         bbs.sort(data, new TestComparator());
         assertArrayEquals(new Integer[]{20, 90, 167, 253, 18976}, data);
     }
+
     @Test
     public void BubbleSort_sortFirstElement_sort() {
         BubbleSort<Integer> bbs = new BubbleSort<>();
@@ -37,6 +41,7 @@ public class Task2_Tests extends Assert {
         bbs.sort(data, new TestComparator());
         assertArrayEquals(new Integer[]{2, 8, 11, 15}, data);
     }
+
     @Test
     public void SelectionSort_sortElement_sort() {
         Integer[] data = {1};
@@ -44,6 +49,7 @@ public class Task2_Tests extends Assert {
         ss.sort(data, new TestComparator());
         assertArrayEquals(new Integer[]{1}, data);
     }
+
     @Test
     public void SelectionSort_sortArray_sort() {
         Integer[] data = {1, 2, 3, 4, 5, 6, 7};
@@ -51,6 +57,7 @@ public class Task2_Tests extends Assert {
         ss.sort(data, new TestComparator());
         assertArrayEquals(new Integer[]{1, 2, 3, 4, 5, 6, 7}, data);
     }
+
     @Test
     public void SelectionSort_RandomInts_sort() {
         Integer[] data = {90, 253, 20, 167, 18976};
@@ -58,6 +65,7 @@ public class Task2_Tests extends Assert {
         ss.sort(data, new TestComparator());
         assertArrayEquals(new Integer[]{20, 90, 167, 253, 18976}, data);
     }
+
     @Test
     public void SelectionSort_sortFirstElement_sort() {
         Sort<Integer> ss = new SelectionSort<>();
@@ -65,6 +73,7 @@ public class Task2_Tests extends Assert {
         ss.sort(data, new TestComparator());
         assertArrayEquals(new Integer[]{2, 8, 11, 15}, data);
     }
+
     @Test
     public void InsertionSort_sortElement_sort() {
         Integer[] data = {1};
@@ -72,11 +81,20 @@ public class Task2_Tests extends Assert {
         is.sort(data, new TestComparator());
         assertArrayEquals(new Integer[]{1}, data);
     }
+
     @Test
     public void InsertionSort_sortArray_sort() {
         Integer[] data = {1, 2, 3, 4, 5, 6, 7};
         Sort<Integer> is = new InsertionSort<>();
         is.sort(data, new TestComparator());
         assertArrayEquals(new Integer[]{1, 2, 3, 4, 5, 6, 7}, data);
+    }
+
+    @Test
+    public void InsertionSort_Random_sort() {
+        Integer[] data = {90, 253, 20, 167, 18976};
+        Sort<Integer> is = new InsertionSort<>();
+        is.sort(data, new TestComparator());
+        assertArrayEquals(new Integer[]{20, 90, 167, 253, 18976}, data);
     }
 }
