6e54cf060d56cf9dfd43b05650bd2e3cd877c16e
jeerryy <90263044+Nata-ui@users.noreply.github.com>
2022-03-29 23:11:47 +0300

    создала тест QuickSort_UnsortedArray_sort и проверила ошибки исправила ошибки и записала их в отчет

--- 2_sem/hw/Task3/127_mirzoeva_task_3/src/Task3_Tests.java
+++ 2_sem/hw/Task3/127_mirzoeva_task_3/src/Task3_Tests.java
@@ -23,4 +23,11 @@ public class Task3_Tests extends Assert {
         qs.sort(arr, new TestComparator());
         assertArrayEquals(new Integer[]{2,2,4,5}, arr);
     }
+    @Test
+    public void QuickSort_UnsortedArray_sort(){
+        Integer[] arr = new Integer[]{2,5,2,3,5};
+        QuickSort<Integer> qs = new QuickSort<>();
+        qs.sort(arr, new TestComparator());
+        assertArrayEquals(new Integer[]{2,2,3,5, 5}, arr);
+    }
 }
