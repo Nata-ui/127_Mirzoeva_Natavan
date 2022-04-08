4eab32dd8695da7e6135a27f325fa836d8de33d0
jeerryy <90263044+Nata-ui@users.noreply.github.com>
2022-04-08 16:25:02 +0300

    создала тест MergeSort_SortBigUnsortedArrayWithNegativeNumbers_DataIsCorrect и проверила ошибки

--- 2_sem/hw/Task3/127_mirzoeva_task_3/src/Task3_Tests.java
+++ 2_sem/hw/Task3/127_mirzoeva_task_3/src/Task3_Tests.java
@@ -93,4 +93,11 @@ public class Task3_Tests extends Assert {
         ms.sort(data, new TestComparator());
         assertArrayEquals(new Integer[]{-1, 0, 1}, data);
     }
+    @Test
+    public void MergeSort_SortBigUnsortedArrayWithNegativeNumbers_DataIsCorrect(){
+        MergeSort<Integer> ms = new MergeSort<>();
+        Integer[] data = {9, 2, 3, 6, 1, -2, 0, 8, 5, 7, 4, -1};
+        ms.sort(data, new TestComparator());
+        assertArrayEquals(new Integer[]{-2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, data);
+    }
 }
