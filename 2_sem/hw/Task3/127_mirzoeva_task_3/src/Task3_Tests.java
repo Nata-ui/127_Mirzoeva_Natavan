f6ee2f8fcd70ad7ce03f31fc111bf4d5a26db607
jeerryy <90263044+Nata-ui@users.noreply.github.com>
2022-04-08 15:26:43 +0300

    создала тест HeapSort_SortSameNumbers_DataIsCorrect, проверила ошибки и исправила их, записав в отчет

--- 2_sem/hw/Task3/127_mirzoeva_task_3/src/Task3_Tests.java
+++ 2_sem/hw/Task3/127_mirzoeva_task_3/src/Task3_Tests.java
@@ -10,103 +10,10 @@ public class Task3_Tests extends Assert {
     }
 
     @Test
-    public void QuickSort_sortTwoElements_sort() {
-        Integer[] arr = new Integer[]{4, 2};
-        QuickSort<Integer> qs = new QuickSort<>();
-        qs.sort(arr, new TestComparator());
-        assertArrayEquals(new Integer[]{2, 4}, arr);
-    }
-
-    @Test
-    public void QuickSort_sortTwoSameElements_sort() {
-        Integer[] arr = new Integer[]{2, 4, 2, 5};
-        QuickSort<Integer> qs = new QuickSort<>();
-        qs.sort(arr, new TestComparator());
-        assertArrayEquals(new Integer[]{2, 2, 4, 5}, arr);
-    }
-
-    @Test
-    public void QuickSort_UnsortedArray_sort() {
-        Integer[] arr = new Integer[]{2, 5, 2, 3, 5};
-        QuickSort<Integer> qs = new QuickSort<>();
-        qs.sort(arr, new TestComparator());
-        assertArrayEquals(new Integer[]{2, 2, 3, 5, 5}, arr);
-    }
-
-    @Test
-    public void MergeSort_sortTwoElements_sort() {
-        Integer[] arr = new Integer[]{4, 2};
-        MergeSort<Integer> qs = new MergeSort<>();
-        qs.sort(arr, new TestComparator());
-        assertArrayEquals(new Integer[]{2, 4}, arr);
-    }
-
-    @Test
-    public void MergeSort_ArrayFirstElements_sort() {
-        Integer[] arr = new Integer[]{3, 2, 4};
-        MergeSort<Integer> qs = new MergeSort<>();
-        qs.sort(arr, new TestComparator());
-        assertArrayEquals(new Integer[]{2, 3, 4}, arr);
-    }
-
-    @Test
-    public void MergeSort_SortArray_ArrayNotChange() {
-        Integer[] arr = new Integer[]{1, 2, 3, 4};
-        MergeSort<Integer> qs = new MergeSort<>();
-        qs.sort(arr, new TestComparator());
-        assertArrayEquals(new Integer[]{1, 2, 3, 4}, arr);
-    }
-
-    @Test
-    public void MergeSort_ArrayTwoSameElements_Sort() {
-        Integer[] arr = new Integer[]{2, 4, 3, 2};
-        MergeSort<Integer> qs = new MergeSort<>();
-        qs.sort(arr, new TestComparator());
-        assertArrayEquals(new Integer[]{2, 2, 3, 4}, arr);
-    }
-
-    @Test
-    public void MergeSort_UnsortedArray_Sort() {
-        Integer[] arr = new Integer[]{1, 5, 2, 5, 4, 2};
-        MergeSort<Integer> qs = new MergeSort<>();
-        qs.sort(arr, new TestComparator());
-        assertArrayEquals(new Integer[]{1, 2, 2, 4, 5, 5}, arr);
-    }
-
-    @Test
-    public void HeapSort_sortTwoElements_sort() {
-        Integer[] arr = new Integer[]{4, 2};
+    public void HeapSort_SortSameNumbers_DataIsCorrect() {
         HeapSort<Integer> hs = new HeapSort<>();
-        hs.sort(arr, new TestComparator());
-        assertArrayEquals(new Integer[]{2, 4}, arr);
-    }
-
-    @Test
-    public void HeapSort_sortArray_ArrayNotChange() {
-        Integer[] arr = new Integer[]{2, 3};
-        HeapSort<Integer> hs = new HeapSort();
-        hs.sort(arr, new TestComparator());
-        assertArrayEquals(new Integer[]{2, 3}, arr);
-    }
-    @Test
-    public void HeapSort_ArrayThreeElements_sort(){
-        Integer[] arr = new Integer[]{3,2,4};
-        HeapSort<Integer> hs = new HeapSort<>();
-        hs.sort(arr, new TestComparator());
-        assertArrayEquals(new Integer[]{2,3,4}, arr);
-    }
-    @Test
-    public void HeapSort_Array_sort(){
-        Integer[] arr = new Integer[]{4,1,3,1,1,3};
-        HeapSort<Integer> hs = new HeapSort<>();
-        hs.sort(arr, new TestComparator());
-        assertArrayEquals(new Integer[]{1,1,1,3,3,4}, arr);
-    }
-    @Test
-    public void HeapSort_Array2_sort(){
-        Integer[] arr = new Integer[]{5,1,2,3,1,5,5,2};
-        HeapSort<Integer> hs = new HeapSort<>();
-        hs.sort(arr, new TestComparator());
-        assertArrayEquals(new Integer[]{1,1,2,2,3,5,5,5}, arr);
+        Integer[] data = {1, 1, 1};
+        hs.sort(data, new TestComparator());
+        assertArrayEquals(new Integer[]{1, 1, 1}, data);
     }
 }
